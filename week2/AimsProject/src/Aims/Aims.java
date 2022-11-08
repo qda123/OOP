6ac48f653297dc57383b82bccee8c742aa3b5fa9
qda123 <98136812+qda123@users.noreply.github.com>
2022-11-08 17:24:32 +0700

    Add files via upload

---
+++ week2/AimsProject/src/Aims/Aims.java
@@ -0,0 +1,9 @@
+package Aims;
+
+public class Aims {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		
+	}
+}
---
+++ week2/AimsProject/src/Aims/Cart.java
@@ -0,0 +1,9 @@
+package Aims;
+
+public class Cart {
+
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] =
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+}
