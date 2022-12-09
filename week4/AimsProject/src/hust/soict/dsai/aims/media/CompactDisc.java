d3f310cd198fec45f8f71c971385c18aa5764011
qda123 <98136812+qda123@users.noreply.github.com>
2022-12-09 22:41:27 +0700

    Add files via upload

---
+++ week4/AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -0,0 +1,94 @@
+package hust.soict.dsai.aims.media;
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc implements Playable {
+	
+    private String artist;
+    private ArrayList<Track> tracks = new ArrayList<>();
+
+    public CompactDisc(){
+    	
+        super();
+        
+    }
+
+    public CompactDisc(String artist, ArrayList<Track> tracks) {
+    	
+        this.artist = artist;
+        this.tracks = tracks;
+        
+    }
+
+    public CompactDisc(String title, String artist, ArrayList<Track> tracks) {
+    	
+        super(title);
+        this.artist = artist;
+        this.tracks = tracks;
+        
+    }
+
+    public CompactDisc(String title, String category, float cost, String artist, ArrayList<Track> tracks) {
+    	
+        super(title, category, cost);
+        this.artist = artist;
+        this.tracks = tracks;
+        
+    }
+
+    public CompactDisc(String title, String category, String director, float cost, String artist, ArrayList<Track> tracks) {
+    	
+        super(title, category, director, cost);
+        this.artist = artist;
+        this.tracks = tracks;
+        
+    }
+
+    public CompactDisc(String title, String category, String director, int length, float cost, String artist, ArrayList<Track> tracks) {
+    	
+        super(title, category, director, length, cost);
+        this.artist = artist;
+        this.tracks = tracks;
+        
+    }
+
+    public String getArtist() {
+    	
+        return artist;
+        
+    }
+
+    public void addTrack(Track a){
+    	
+        if(!tracks.contains(a)){
+        	
+            tracks.add(a);
+            
+        }
+        
+    }
+    
+    public void removeTrack(Track a){
+    	
+        tracks.remove(a);
+        
+    }
+
+    public int getLength(){
+    	
+        return length;
+        
+    }
+    
+    public void play() {
+    	
+        System.out.println("Artist: " + this.artist);
+        System.out.println("List of track");
+        for(Track a:tracks){
+        	
+            a.play();
+            
+        }
+        
+    }
+    
+}
